5979068aaf5f6b034dc8be6805e23fd27ebe743b
TgateCS <61422887+TgateCS@users.noreply.github.com>
2022-10-05 08:31:40 -0500

    added a couple unit tests

--- front-end/movie-cards/src/app/app.component.spec.ts
+++ front-end/movie-cards/src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('movie-cards');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('movie-cards app is running!');
-  });
 });
--- front-end/movie-cards/src/app/movies.service.spec.ts
+++ front-end/movie-cards/src/app/movies.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { MovieService } from './movies.service';
 
-import { MoviesService } from './movies.service';
+import films from '../../../../film.json';
 
 describe('MoviesService', () => {
-  let service: MoviesService;
+  let service: MovieService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(MoviesService);
+    service = TestBed.inject(MovieService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should get the movies list from the server'), () =>{
+    let actual;
+    service.getHttpMovies().subscribe(movies => actual = movies);
+    expect(actual).toContain(films);
+  }
+
+  it('should post to server and return posted list'), () => {
+    let actual = service.saveMovies(JSON.parse(JSON.stringify(films)));
+    expect(actual).toContain(films);
+  }
 });
